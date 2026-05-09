32d6a8b0a2e2fbf59f80619bebd6133a80238942
Pamela <pamela.santana@goop.com.br>
2026-05-09 11:32:45 -0300

    Fix: resolvendo erro de namespace no domínio e refinando escopo de build no CI

--- Almoxarifado.Domain/Interfaces/PedidoExtensions.cs
+++ Almoxarifado.Domain/Interfaces/PedidoExtensions.cs
@@ -1,5 +1,7 @@
 namespace Almoxarifado.Domain.Interfaces;
 
+using Almoxarifado.Domain;
+
 public static class PedidoExtensions
 {
     public static async Task<IEnumerable<Pedido>> GetByUserIdAsync(this IReadOnlyRepository<Pedido> repository, int userId)
